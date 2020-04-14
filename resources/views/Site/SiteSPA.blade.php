26336f3df712ad3d9cc089d7cdb26647ed217344
Matheus365 <matheus2006_dias@hotmail.com>
2020-04-13 22:40:43 -0300

    Efeito Parallax, base da sessão de sobre

--- resources/views/Site/SiteSPA.blade.php
+++ resources/views/Site/SiteSPA.blade.php
@@ -3,18 +3,44 @@
 @section('content')
 
 <style>
-    .content-site {
-       
-    }
+  .hero1 {
+    background-image: url('../../../images/foto1.jpeg');
+  }
+  .hero2 {
+    background-image: url('../../../images/foto3.jpg');
+  }
+  .hero3 {
+    background-image: url('../../../images/foto2.jpg');
+  }
+  .paralax {
+    background-attachment: fixed;
+    height: 60%;
+    width: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  #sobre {
+    height: 80%;
+  }
 </style>
 
 <div class="content-site">
-    <div class="container">
-      @include('Site.sobre.sobre')
-    </div>
+      <div id="sobre">
+        @include('Site.sobre.sobre')
+      </div>
+      <div class="hero1 paralax"></div>
+      <div id="sobre">
+        @include('Site.sobre.sobre')
+      </div>
+      <div class="hero2 paralax"></div>
+      <div id="sobre">
+        @include('Site.sobre.sobre')
+      </div>
+      <div class="hero3 paralax"></div>
+      <div id="sobre">
+        @include('Site.sobre.sobre')
+      </div>
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="{{asset('js/parallax.min.js')}}"></script>
 
 @endsection
--- resources/views/Site/sobre/sobre.blade.php
+++ resources/views/Site/sobre/sobre.blade.php
@@ -1,1 +1,51 @@
-<h1>Sobre</h1>
+<style>
+    .text_info {
+        color: #B06F1A;
+        text-justify: left;
+    }
+    .separator {
+        height: 1px;
+        width: 100%;
+        background-color: #B06F1A;
+    }
+    .section_text {
+        padding-bottom: 50px;
+    }
+    .section_text h2{
+        color: white;
+        text-align: center;
+    }
+    .filosofia {
+        float: right;
+    }
+    .space_photo {
+        width: 100%;
+        height: 30%;
+        background-color: white;
+    }
+</style>
+<div class="container">
+    <div class="section_text">
+        <h2>Sobre Nós</h2>
+    </div>
+    <div class="row">
+        <div class="col-8">
+            <div class="space filosofia">
+                <h4 class="text_info">Filosofia</h4>
+                <hr class="separator">
+                <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex sequi illo debitis consectetur laboriosam ut? Modi ratione, fugiat harum, nesciunt voluptatum rerum, nisi ipsam minus corrupti beatae similique maiores. Hic.</p>
+            </div>
+            <div class="space funcionamento">
+                <h4 class="text_info">Funcionamento</h4>
+                <hr class="separator">
+                <p class="description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, autem. Veniam, voluptatibus. Veritatis, voluptatibus! Veritatis blanditiis recusandae molestiae nulla aperiam excepturi. Consequuntur sed eos repellat rerum nulla aperiam, commodi blanditiis.</p>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="space_photo image_owner">
+                <img src="{{asset('images/owner.jpg')}}" alt="">
+            </div>
+        </div>
+        
+    </div>
+</div>
