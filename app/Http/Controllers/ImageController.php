cd0be42e3b13a97232fb64f886e25cb5f6b1dd0d
Eduardo <eduardogomes.2607@hotmail.com>
2020-05-03 01:07:56 -0300

    Protecao de rotas

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/ProfissionalController.php
+++ app/Http/Controllers/ProfissionalController.php
@@ -10,6 +10,12 @@ use Illuminate\Support\Facades\Storage;
 
 class ProfissionalController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
